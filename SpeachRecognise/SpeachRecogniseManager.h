124a1814546a22c31494dd14d63cdd817d90b2e2
Adnan Hasan <adnan@Adnans-MacBook-Pro.local>
2018-05-30 01:51:53 +0600

    first commit

---
+++ SpeachRecognise/SpeachRecogniseManager.h
@@ -0,0 +1,13 @@
+//
+//  SpeachRecogniseManager.h
+//  SpeachRecognise
+//
+//  Created by Adnan Hasan on 5/29/18.
+//  Copyright © 2018 Adnan Hasan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpeachRecogniseManager : NSObject
+
+@end
---
+++ SpeachRecognise/SpeachRecogniseManager.m
@@ -0,0 +1,13 @@
+//
+//  SpeachRecogniseManager.m
+//  SpeachRecognise
+//
+//  Created by Adnan Hasan on 5/29/18.
+//  Copyright © 2018 Adnan Hasan. All rights reserved.
+//
+
+#import "SpeachRecogniseManager.h"
+
+@implementation SpeachRecogniseManager
+
+@end
